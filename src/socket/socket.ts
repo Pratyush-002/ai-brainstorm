137b162a23890d8b1f02b7f0ce1a8c42f7176e9f
Pratyush-Telcovas <pratyush.anand@telcovasworld.com>
2026-03-14 12:38:07 +0530

    socket fix

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -2,4 +2,7 @@ import { io } from "socket.io-client";
 
 export const socket = io("https://ai-brainstrom-backend.onrender.com", {
   autoConnect: true,
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 2000
 });
